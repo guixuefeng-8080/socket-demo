86fa779760d5b9f72ef08a3a686ba479caf7e540
SnowPackXF <guixuefeng2020@163.com>
2021-01-23 22:36:19 +0800

    tcpdemo

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,17 +1,19 @@
 import java.io.*;
-import java.net.Inet4Address;
-import java.net.InetSocketAddress;
-import java.net.Socket;
+import java.net.*;
 
 public class Client {
 
+    public static final int PORT = 20000;
+    public static final int PORT_LOCATION = 20001;
 
     public static void main(String[] args) throws IOException {
 
-        Socket socket = new Socket();
-        socket.setSoTimeout(3000);
-        //连接本地端口 2000，超时3000ms
-        socket.connect(new InetSocketAddress(Inet4Address.getLocalHost(),2000),3000);
+
+
+        Socket socket = creatSocket();
+        initSocket(socket);
+        //客户端连接
+        socket.connect(new InetSocketAddress(Inet4Address.getLocalHost(),PORT),3000);
         System.out.println("已经连接成功，并进入后续流程~");
         System.out.println("客户端信息："+socket.getLocalAddress()+"P:"+socket.getLocalPort());
         System.out.println("服务器端信息："+socket.getInetAddress()+"P:"+socket.getPort());
@@ -25,6 +27,49 @@ public class Client {
         socket.close();
         System.out.println("客户端关闭~");
     }
+
+    private static void initSocket(Socket socket) throws SocketException {
+        //设置 读取 超时 时间
+        socket.setSoTimeout(2000);
+        //正常情况下，端口一旦被bind即使连接关闭，2min之内不能被重新bind，而设置为true的话，一旦连接完全关闭，端口就可以重新分配
+        socket.setReuseAddress(true);
+        //是否开启Nagle算法。优化网络空间，减少ACK次数。不是每次数据片收到后都发ACK，是攒到一定数量后一起发ACK
+        socket.setTcpNoDelay(true);
+        //心跳检测，2H内没有数据发送，则会发送一条数据，如果没有收到ACK则认为连接断开，会抛出异常
+        socket.setKeepAlive(true);
+        //对于close关闭操作怎样进行
+        //1.false 0,默认。close时立即返回；底层系统接管输出流，将缓冲区的数据发送完成。
+        //2.true 0,close时立即返回，缓冲区数据抛弃，直接发送RST结束命令到对放，无需经过2个MSL(数据等待时间)
+        //3.true 2000,close时阻塞2S,随后按2.处理
+        socket.setSoLinger(true,2000);
+        //设置发送和接收缓冲区大小//默认32K
+        socket.setReceiveBufferSize(64*1024*1024);
+        socket.setSendBufferSize(64*1024*1024);
+        //设置性能参数  连接时间，延迟，带宽的比例
+        //如果建立的是一个比较长的链接，比如要传个文件，需要传一会。则connectionTime权重要设的高一些
+        //如果对延迟不太能接受，要及时的送达，则把latency的权重设置的高一些
+        //如果能接受延迟，则要把带宽提高，第一个包组装完成先不发，等第二个一起发，组包发送，带宽利用率较高
+        socket.setPerformancePreferences(1,1,1);
+    }
+
+    private static Socket creatSocket() throws IOException {
+
+        //代理模式
+                                //代理模式       //代理的IP及端口
+        //Proxy proxy = new Proxy(Proxy.Type.HTTP,new InetSocketAddress("172.31.150.66",2000));
+        //Socket socket = new Socket(proxy);
+
+        //无代理模式
+        //创建一个socket，并且直接连接到本地服务端20000端口上
+        //Socket socket = new Socket(Inet4Address.getLocalHost(),PORT);
+                                   //远程的地址                //远程的端口  //本地的地址            //本地的端口
+        //Socket socket = new Socket(Inet4Address.getLocalHost(),PORT,Inet4Address.getLocalHost(),PORT_LOCATION);
+        //等效
+        Socket socket = new Socket();
+        socket.bind(new InetSocketAddress(Inet4Address.getLocalHost(),PORT_LOCATION));
+        return socket;
+    }
+
     private static void todo(Socket client)throws IOException{
         //构建键盘输入流
         InputStream in  = System.in;
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,27 +1,46 @@
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 
 public class Server {
-
+    public static final int PORT = 20000;
     public static void main(String[] args) throws IOException {
 
-        //创建服务端，一般服务端都是在本机，所以只需提供端口。如果本机处于复杂的网络中，需要指定IP
-        ServerSocket server = new ServerSocket(2000);
-
+        ServerSocket serverSocket = creatServerSocket();
+        initServerSocket(serverSocket);
+        //backlog:允许等待的最大连接队列，第51个客户端连接时将有异常
+        serverSocket.bind(new InetSocketAddress(Inet4Address.getLocalHost(),PORT),50);
         System.out.println("服务端准备就绪~");
-        System.out.println("服务端信息："+server.getInetAddress()+"P:"+server.getLocalPort());
+        System.out.println("服务端信息："+serverSocket.getInetAddress()+"P:"+serverSocket.getLocalPort());
 
         while (true){
 
             //等待客户端的连接
-            Socket client = server.accept();
+            Socket client = serverSocket.accept();
             //客户端构建异步线程
             ClientHandle clientHandle = new ClientHandle(client);
             //启动线程
             clientHandle.start();
 
         }
+    }
+
+    private static void initServerSocket(ServerSocket serverSocket) throws SocketException {
+        //close时IP和端口并没有释放，true是立刻释放
+        serverSocket.setReuseAddress(true);
+        //设置接收缓冲区大小,即服务端接收到了一个客户端时给这个客户端设置了接收缓冲区大小//需要在拿到客户端之前设置
+        serverSocket.setReceiveBufferSize(64*1024*1024);
+        //设置serverSocket的accept的超时时间//超时则会抛异常，如果时无限循环则可以处理异常后再进行accept//一般不设置超时时间
+        //serverSocket.setSoTimeout(2000);
+        //设置性能参数//服务端得到客户端之前设置，所以只能设置到serverSocket上
+        serverSocket.setPerformancePreferences(1,1,1);
+    }
+
+    private static ServerSocket creatServerSocket() throws IOException {
+        ServerSocket serverSocket = new ServerSocket();
+        //等效
+        //创建本地的服务端Socket，并绑定
+        //ServerSocket serverSocket = new ServerSocket(PORT_LOCATION,50);
+        return serverSocket;
     }
 
     private static class ClientHandle extends Thread{
